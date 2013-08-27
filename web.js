40acaef76ce3c94aea05830ff436609963427d2a
njdup <njdupoux@stanford.edu>
2013-08-27 11:07:27 -0400

    Added node.js app to serve site.

---
+++ web.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+
+
+var app = express();
+var filename = "home.html";
+var content = fs.readFileSync(filename);
+var converted = content.toString();
+
+app.use(express.static(__dirname));
+
+app.get('/', function(request, response){
+       	response.send(converted);
+    });
+
+var port = process.env.port || 8080;
+app.listen(port, function(){
+	console.log("Listening on " + port);
+    });
