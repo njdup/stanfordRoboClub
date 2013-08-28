55066b141e38a6e6140a97d52d8f75d5a1c619b9
njdup <njdupoux@stanford.edu>
2013-08-28 17:53:34 -0400

    Edited web.js

--- web.js
+++ web.js
@@ -8,13 +8,13 @@ var filename = "home.html";
 var content = fs.readFileSync(filename);
 var converted = content.toString();
 
-//app.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
 app.get('/', function(request, response){
        	response.send(converted);
     });
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 app.listen(port, function(){
 	console.log("Listening on " + port);
     });
